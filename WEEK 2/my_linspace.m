ff48de7a3fec50c7668ccfe4ba9ecd32ff95e540
Edmond V <67634227+edvujic@users.noreply.github.com>
2020-10-25 14:49:17 +0100

    my_linspace.m without a for loop

---
+++ WEEK 2/my_linspace.m
@@ -0,0 +1,21 @@
+% CREATE YOUR OWN LINSPACE FUNCTION
+% y = linspace(x1,x2,n) generates n points. The spacing between the points is (x2-x1)/(n-1).
+% linspace is similar to the colon operator, “:”, but gives direct control over the number of points and always includes the endpoints. “lin” in the name “linspace” refers to generating linearly spaced values as opposed to the sibling function logspace, which generates logarithmically spaced values.
+% y = linspace(x1,x2,n) generates n points. The spacing between the points is (x2-x1)/(n-1).
+% linspace is similar to the colon operator, “:”, but gives direct control over the number of points and always includes the endpoints. 
+% “lin” in the name “linspace” refers to generating linearly spaced values as opposed to the sibling function logspace, which generates logarithmically spaced values.
+function [outputArg] = my_linspace(x1,x2,num_el)
+    switch nargin
+        %check for # of inputs
+        %girdi sayısını kontrol et
+        case 2
+            n = 100;
+            incrementation = (x2 - x1)/(n - 1);
+            outputArg = x1: incrementation: x2;
+            %use matrix declaration properties
+            %matris oluşturma yöntemleriyle matrisi oluştur
+        case 3
+            incrementation = (x2 - x1)/(num_el - 1);
+            outputArg = x1: incrementation: x2;
+
+end
